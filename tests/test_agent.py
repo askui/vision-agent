a160fc8f02650a1880ac6798ce488933cae0a1f3
Maximilian Wessendorf <maximilian.wessendorf@askui.com>
2024-10-28 10:44:11 +0100

    comment tests

--- tests/test_agent.py
+++ tests/test_agent.py
@@ -18,9 +18,19 @@ import time
 #     agent.close()
 
 
-def test_agent_act():
-    agent = VisionAgent()
-    agent.cli("firefox")
-    time.sleep(1)
-    agent.act("search for flights")
-    assert False
+# def test_message():
+#     agent = VisionAgent()
+#     agent.click("Maxi du")
+#     agent.click("Nachricht an")
+#     agent.type("This message is sent with AGI")
+#     agent.keyboard("enter")
+#     agent.close()
+
+
+# def test_agent_act():
+#     agent = VisionAgent()
+#     agent.cli("firefox")
+#     time.sleep(1)
+#     agent.act("search for a flight from frankfurt to kapstadt in January")
+#     agent.close()
+#     assert False
