e8d46926d3a7e443319022cec30a95434351d2b3
Maximilian Wessendorf <maximilian.wessendorf@askui.com>
2024-11-14 11:01:03 +0100

    added pyperclip for clipboard control

--- src/askui/tools/toolbox.py
+++ src/askui/tools/toolbox.py
@@ -1,9 +1,11 @@
+import pyperclip
 import webbrowser
 
 
 class AgentToolbox:
     def __init__(self):
         self.webbrowser: webbrowser = webbrowser
+        self.pyperclip: pyperclip = pyperclip
 
     def list_tools(self):
         return self.__dict__
