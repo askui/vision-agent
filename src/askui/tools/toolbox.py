b19ab6465e5d9f919fd746c9c63ea4e042007c0f
Maximilian Wessendorf <maximilian.wessendorf@askui.com>
2024-11-15 14:07:31 +0100

    generic name for clipboard tool

--- src/askui/tools/toolbox.py
+++ src/askui/tools/toolbox.py
@@ -5,7 +5,7 @@ import webbrowser
 class AgentToolbox:
     def __init__(self):
         self.webbrowser: webbrowser = webbrowser
-        self.pyperclip: pyperclip = pyperclip
+        self.clipboard: pyperclip = pyperclip
 
     def list_tools(self):
         return self.__dict__
