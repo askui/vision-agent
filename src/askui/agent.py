2d62ac1cbd3999e38fcf9f246f384d6693c65465
Adrian Stritzinger <Adrian.Stritzinger@askui.com>
2025-03-27 14:12:24 +0100

    fix(agent): readd reporting statement lost in rebasing

--- src/askui/agent.py
+++ src/askui/agent.py
@@ -89,6 +89,8 @@ class VisionAgent:
                 msg = f'{button} ' + msg 
             if repeat > 1:
                 msg += f' {repeat}x times'
+            if instruction is not None:
+                msg += f' on "{instruction}"'
             self.report.add_message("User", msg)
         if instruction is not None:
             logger.debug("VisionAgent received instruction to click '%s'", instruction)
