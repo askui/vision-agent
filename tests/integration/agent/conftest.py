8078dc2491c4569f1cca563a3b24868e59963e57
onur-askui <210008309+onur-askui@users.noreply.github.com>
2025-05-20 11:53:38 +0200

    test: add agent settings and fix formating

--- tests/integration/agent/conftest.py
+++ tests/integration/agent/conftest.py
@@ -1,12 +1,13 @@
 from typing import Generator, Optional, Union
 
-from askui.models.askui.claude_computer_agent import AskUiClaudeComputerAgent
-from askui.tools.agent_os import AgentOs
 import pytest
 from PIL import Image as PILImage
 from typing_extensions import override
 
+from askui.models.askui.claude_computer_agent import AskUiClaudeComputerAgent
+from askui.models.askui.settings import AskUiClaudeComputerAgentSettings, AskUiSettings
 from askui.reporting import Reporter
+from askui.tools.agent_os import AgentOs
 
 
 class ReporterMock(Reporter):
@@ -23,13 +24,17 @@ class ReporterMock(Reporter):
     def generate(self) -> None:
         pass
 
+
 @pytest.fixture
 def claude_computer_agent(
     agent_os_mock: AgentOs,
 ) -> Generator[AskUiClaudeComputerAgent, None, None]:
     """Fixture providing a AskUiClaudeComputerAgent instance."""
-    agent = AskUiClaudeComputerAgent(
-        agent_os=agent_os_mock,
-        reporter=ReporterMock(),
+    agent = (
+        AskUiClaudeComputerAgent(
+            agent_os=agent_os_mock,
+            reporter=ReporterMock(),
+            settings=AskUiClaudeComputerAgentSettings(askui=AskUiSettings()),
+        ),
     )
     yield agent
