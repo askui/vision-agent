f7070b67ddb8a2618ef560283da2315325ba79b8
Adrian Stritzinger <Adrian.Stritzinger@askui.com>
2025-07-07 13:26:19 +0200

    feat: run chat api with `python -m askui.chat`
    also:
    - make default chat api port consistent to allow
      for easier testing

--- src/askui/chat/api/settings.py
+++ src/askui/chat/api/settings.py
@@ -15,3 +15,17 @@ class Settings(BaseSettings):
         default_factory=lambda: Path.cwd() / "chat",
         description="Base directory for storing chat data",
     )
+    host: str = Field(
+        default="127.0.0.1",
+        description="Host for the chat API",
+    )
+    log_level: str | int = Field(
+        default="info",
+        description="Log level for the chat API",
+    )
+    port: int = Field(
+        default=9261,
+        description="Port for the chat API",
+        ge=1024,
+        le=65535,
+    )
