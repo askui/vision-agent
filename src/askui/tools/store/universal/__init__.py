347a0c18485b3de05af7517e035734e7bea1be9f
Samir Mlika <samir.mlika@askui.com>
2026-01-12 18:17:10 +0100

    add file list and read tools to the store

--- src/askui/tools/store/universal/__init__.py
+++ src/askui/tools/store/universal/__init__.py
@@ -4,10 +4,14 @@ These tools don't require agent_os and can be used with VisionAgent,
 AndroidVisionAgent, or any other agent type.
 """
 
+from .list_files_tool import ListFilesTool
 from .print_to_console import PrintToConsoleTool
+from .read_from_file_tool import ReadFromFileTool
 from .write_to_file_tool import WriteToFileTool
 
 __all__ = [
+    "ListFilesTool",
     "PrintToConsoleTool",
+    "ReadFromFileTool",
     "WriteToFileTool",
 ]
